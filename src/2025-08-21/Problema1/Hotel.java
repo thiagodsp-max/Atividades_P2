8d6b8f02e826351a937a4bc337a2354b3145bfe0
thiagodsp-max <tsp@ic.ufal.br>
2025-08-21 00:27:07 -0300

    Create Hotel.java

---
+++ src/2025-08-21/Problema1/Hotel.java
@@ -0,0 +1,16 @@
+package Atividade_31;
+import java.util.Scanner;
+
+public class Hotel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner read= new Scanner(System.in);
+		System.out.print("Digite o número do quarto:");
+		int room=read.nextInt();
+		read.nextLine();
+		
+		//
+	}
+
+}
